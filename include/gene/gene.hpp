70e688a85dfc80540d8b3a5c3b6a13e991a93dda
rhysd <lin90162@yahoo.co.jp>
2013-03-08 11:49:23 +0900

    header which includes all headers

---
+++ include/gene/gene.hpp
@@ -0,0 +1,8 @@
+#if !defined GENE_GENE_HPP_INCLUDED
+#define      GENE_GENE_HPP_INCLUDED
+
+#include "gene/config.hpp"
+#include "gene/node.hpp"
+#include "gene/operators.hpp"
+
+#endif    // GENE_GENE_HPP_INCLUDED
